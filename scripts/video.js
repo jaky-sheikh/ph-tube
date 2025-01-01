a97d1a7f9bedffaab52893f85a42eae856f2bc75
jaky-sheikh <jaky@eu4m.eu>
2025-01-01 22:51:55 +0600

    display videos dynamically

--- scripts/video.js
+++ scripts/video.js
@@ -4,6 +4,35 @@ const loadCategories = () => {
         .then(data => displayCategories(data.categories))
         .catch((error) => console.log(error));
 }
+const loadVideos = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+        .then(res => res.json())
+        .then(data => displayVideos(data.videos))
+        .catch((error) => console.log(error));
+}
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById('videos');
+    videos.forEach((video) => {
+        console.log(video);
+        const card = document.createElement('div');
+        card.classList = 'card card-compact';
+        card.innerHTML = `
+        <figure>
+    <img
+      src=${video.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+        `;
+        videoContainer.appendChild(card);
+    })
+}
 
 const displayCategories = (categories) => {
 
@@ -22,3 +51,4 @@ const displayCategories = (categories) => {
 
 
 loadCategories();
+loadVideos();
